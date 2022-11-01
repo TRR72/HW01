2b3e2c5bcd0b6625c2b240cdad3a2fcf5e532ee5
TRR72 <116944430+TRR72@users.noreply.github.com>
2022-11-01 13:16:16 +0500

    HomeWork02

---
+++ HW02/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+double a = 22, b = 3, c = 9;
+            
+                if (a > b && a > c)
+                    Console.WriteLine("Nubmer Max = " + a); 
+                else if (b > c)
+                    Console.WriteLine("Number Max = " + b); 
+                else
+                    Console.WriteLine("Number Max = " + c); 
+            
+            Console.ReadLine();
