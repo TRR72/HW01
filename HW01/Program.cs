1787fd9d175450765a2261cebb218f1d920cdd3f
TRR72 <116944430+TRR72@users.noreply.github.com>
2022-11-01 13:14:54 +0500

    HomeWork01

---
+++ HW01/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int a = -9;
+int b = -3;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+Console.Write("max = ");
+Console.WriteLine(max);
